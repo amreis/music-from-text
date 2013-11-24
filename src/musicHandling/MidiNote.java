0912bfa1d5298c7f26e0689625c76c285be93d01
Alister Machado <alister.reis@gmail.com>
2013-11-24 16:35:51 -0200

    Removed old code.

--- src/musicHandling/MidiNote.java
+++ src/musicHandling/MidiNote.java
@@ -10,7 +10,6 @@ package musicHandling;
 
 public abstract class MidiNote
 {
-	private static final int CENTRAL_OCTAVE = 5;
 	private static final int NOTES_IN_AN_OCTAVE = 12;
 	
 	public static int midiValue(int noteIndex, int octave)
@@ -18,100 +17,3 @@ public abstract class MidiNote
 		return noteIndex + octave * NOTES_IN_AN_OCTAVE;
 	}
 }
-/*
-public abstract class MidiNote {
-    DO(60),
-    DO_SHARP(61),
-    RE(62),
-    RE_SHARP(63),
-    MI(64),
-    FA(65),
-    FA_SHARP(66),
-    SOL(67),
-    SOL_SHARP(68),
-    LA(69),
-    LA_SHARP(70),
-    SI(71);
-    
-    private int midiValue;
-    public static final int CENTRAL_OCTAVE = 5;
-    public static final int NOTES_IN_AN_OCTAVE = 12;
-
-    MidiNote(int value)
-    {
-        this.midiValue = value;
-    }
-    
-  
-    public int getMidiValue()
-    {
-        return this.midiValue;
-    }
-    
-
-    public static MidiNote fromCharValue(char midiValue)
-    {
-        switch(midiValue)
-        {
-            case 'c':
-                return DO;
-            case 'd':
-                return RE;
-            case 'e':
-                return MI;
-            case 'f':
-                return FA;
-            case 'g':
-                return SOL;
-            case 'a':
-                return LA;
-            case 'b':
-                return SI;
-        }
-        return null;
-    }
-    
-    public static MidiNote fromMidiValue(int midiValue) throws Exception
-    {
-        if (midiValue < 0)
-            throw new Exception("Value is not valid for a note");
-        switch (midiValue % NOTES_IN_AN_OCTAVE)
-        {
-            case 0:
-                return DO;
-            case 1:
-                return DO_SHARP;
-            case 2:
-                return RE;
-            case 3:
-                return RE_SHARP;
-            case 4:
-                return MI;
-            case 5:
-                return FA;
-            case 6:
-                return FA_SHARP;
-            case 7:
-                return SOL;
-            case 8:
-                return SOL_SHARP;
-            case 9:
-                return LA;
-            case 10:
-                return LA_SHARP;
-            case 11:
-                return SI;
-            default:
-                ; // Never Happens.
-        }
-        
-        return null;
-    }
-    
-    public int getMidivalueWithOctave(int octave)
-    {
-        int value = this.midiValue + (octave - CENTRAL_OCTAVE) * NOTES_IN_AN_OCTAVE;
-        if (value < 0) value = 0;
-        return value;
-    }
-}*/
